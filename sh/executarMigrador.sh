efecbc8b08748b31cca462b1c91f12423a405b5d
Administrador servidor <geral@agil.inf.ufsc.br>
2015-08-11 18:21:40 -0300

    Adição de script para executar migrador.

---
+++ sh/executarMigrador.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd $1
+bibliotecas=`find $1/jar -name "*.jar" | paste -s -d ":"`
+java -classpath $bibliotecas:$1/binarios br.ufsc.inf.leb.projetos.migrador.MigradorDeProjetosParaPastaEspecifica
