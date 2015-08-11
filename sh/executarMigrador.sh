d30e5067e73cd0bd53bde498640b1a3eb0652c47
Administrador servidor <geral@agil.inf.ufsc.br>
2015-08-11 20:21:55 -0300

    Atualizando do script migrador.

--- sh/executarMigrador.sh
+++ sh/executarMigrador.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd $1
-migrador=MigradorRemovedorDePrefixoDosProjetos
+migrador=MigradorRenomeadorDeArquivosZip
+#migrador=MigradorRemovedorDePrefixoDosProjetos
 #migrador=MigradorDeProjetosParaPastaEspecifica
 bibliotecas=`find $1/jar -name "*.jar" | paste -s -d ":"`
 java -classpath $bibliotecas:$1/binarios br.ufsc.inf.leb.projetos.migrador.${migrador}
